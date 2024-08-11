b2ac15e643b5134febe1408f206ff4f2ddce5582
= <=>
2024-08-12 01:47:38 +0530

    used getchar and a little explanation of casting

---
+++ getchar.cpp
@@ -0,0 +1,26 @@
+#include <iostream>  //including the input/output stream or directory
+#include <string>    //inclusign the string functions from string directory
+using namespace std; // used for including prefixvstd:: for cout/cin etc.
+
+// the main fuction is the start of our code.
+int main()
+{
+    cout << "Do you want to play a game? (y/n): ";
+    char response; //here we can also use int DATATYPE
+
+    response = getchar(); //getchar is a function to read an input form the user.
+    
+    // here we are changing the varibale response from char to int DATATYPE
+    // int r = static_cast<int> (response);
+    cin >> response;
+    cout << "You entered: " << response << "\n";
+
+
+// we use if condition to check for certain conditions.
+
+// we used comparision operator to compare the truthy and falsey value.
+    if(response == 'y') {
+        cout << "Let's play a game then...\n";
+    }
+    return 0;
+}
